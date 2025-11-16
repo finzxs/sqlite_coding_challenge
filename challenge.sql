27e5c475eda1b2ec6215fd6f265a70b43ad6d807
Hayden <haydenfinney@usf.edu>
2025-11-16 16:13:17 -0500

    above average salary employees

--- challenge.sql
+++ challenge.sql
@@ -24,4 +24,36 @@ JOIN orders o
 GROUP BY p.category
 ORDER BY revenue DESC;
 
+SELECT
+    p.category,
+    SUM(oi.quantity * oi.unit_price) AS revenue
+FROM order_items oi
+JOIN products p
+    ON p.id = oi.product_id
+JOIN orders o
+    ON o.id = oi.order_id
+WHERE o.status = 'Delivered'
+GROUP BY p.category
+ORDER BY revenue DESC;
 
+--step 3: Above average salary employees
+WITH dept_avg AS (
+    SELECT
+        department_id,
+        AVG(salary) AS avg_salary
+    FROM employees
+    GROUP BY department_id
+)
+SELECT
+    e.first_name,
+    e.last_name,
+    d.name AS department_name,
+    e.salary AS employee_salary,
+    dept_avg.avg_salary AS department_average_salary
+FROM employees e
+JOIN dept_avg
+    ON dept_avg.department_id = e.department_id
+JOIN departments d
+    ON d.id = e.department_id
+WHERE e.salary > dept_avg.avg_salary
+ORDER BY d.name, e.salary DESC;
