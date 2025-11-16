fcc59fcca2270ab7d5c95f11bc76c52271397aef
Hayden <haydenfinney@usf.edu>
2025-11-16 16:09:53 -0500

    top5customersquery

--- challenge.sql
+++ challenge.sql
@@ -1,1 +1,16 @@
-SELECT * FROM customers LIMIT 5;
+--step 1: Write a SQL query to find the top 5 customers who have spent the most money on orders.
+
+SELECT
+    c.first_name || ' ' || c.last_name AS customer_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o
+    ON o.customer_id = c.id
+JOIN order_items oi
+    ON oi.order_id = o.id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;SELECT * FROM customers LIMIT 5;
+
+--step 2: 
+
